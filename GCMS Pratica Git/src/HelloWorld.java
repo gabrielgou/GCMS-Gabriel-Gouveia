4c2e2ecc92bac4a16de819dab35b82bf4bf42a6b
gabri <gabri@LAPTOP-S9VMCB77>
2022-06-30 23:02:06 -0300

    Ultimo commit de teste

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Pratica 3");
+		System.out.println("Novo Push");
 	}
 
 }
