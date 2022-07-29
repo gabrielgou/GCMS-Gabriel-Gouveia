a5ba5e8dc3c755f81f40c8f485238a49353a5b7b
gabrielgou <gabriel.gouveia@live.com>
2022-07-29 00:58:06 -0300

    Adicionar println de teste Hotfix

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Pratica 3");
 		System.out.println("Commit local 1");
 		System.out.println("Commit local "+" 2");
+		System.out.println("teste branch hotfix");
 	}
 
 }
